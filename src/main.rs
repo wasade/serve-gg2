fb208b3f08582c8bd2955bb5908118bbd3c7d9cf
Daniel McDonald <danielmcdonald@ucsd.edu>
2022-09-16 15:01:24 -0700

    tab in

--- src/main.rs
+++ src/main.rs
@@ -43,12 +43,12 @@ fn get_server() -> String {
 #[tokio::main]
 async fn main() {
     let subscriber = tracing_subscriber::fmt()
-    .compact()
-    .with_file(true)
-    .with_line_number(true)
-    .with_thread_ids(true)
-    .with_target(false)
-    .finish();
+        .compact()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_target(false)
+        .finish();
     tracing::subscriber::set_global_default(subscriber).unwrap();
 
     let args = Args::parse();
